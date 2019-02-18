56ba6fbbc09aebafe0c2ad7b4b767e8861a093ec
Matt Lindsey <waypastspooked@gmail.com>
2019-02-17 19:57:26 -0500

    CSS Updates

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
   <![endif]-->
 
-  <nav class="navbar navbar-expand-lg">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   	<a class="navbar-brand" href="index.html"><img class="logo" src="img/AMSLogo360White.png" alt="AMS 360"></a>
 
   	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,7 +34,7 @@
   	<div class="collapse navbar-collapse" id="navbarSupportedContent">
     	<ul class="navbar-nav mr-auto">
       	<li class="nav-item">
-      		<a class="nav-link active" href="index.html">Home</a>
+      		<a class="nav-link" href="index.html">Home</a>
       	</li>
       	<li class="nav-item">
         	<a class="nav-link" href="training.html">Training</a>
@@ -43,143 +43,148 @@
         	<a class="nav-link" href="about.html">About</a>
       	</li>
       	<li class="nav-item">
-        	<a class="nav-link" href="contact.php">Contact</a>
+        	<a class="nav-link active" href="contact.php">Contact</a>
       	</li>
     	</ul>
   	</div>
 	</nav>
 
   <header>
-    <h1>Welcome!</h1>
+    <h1>Email AMS 360</h1>
   </header>
 
   <section class="clearfix">
-    <div class="contentText">
-      <h2>EMAIL FORM</h2>
+    <p>For general inquires:</p>
 
-      <?php
-        if(isset($_POST['email'])) {
-          $email_to = "waypastspooked@gmail.com";
-          $email_subject = "Message from ams360inc.com";
-          
-          // validation expected data exists
-          if(!isset($_POST['first_name']) ||
-            !isset($_POST['last_name']) ||
-            !isset($_POST['email']) ||
-            !isset($_POST['telephone']) ||
-            !isset($_POST['comments'])) {
-            echo '<p class="mailError">We are sorry, but there appears to be a problem with the form you submitted.</p>';
-          }
-     
-          $first_name = $_POST['first_name']; // required
-          $last_name = $_POST['last_name']; // required
-          $email_from = $_POST['email']; // required
-          $telephone = $_POST['telephone']; // not required
-          $comments = $_POST['comments']; // required
-     
-          $error_message = "";
-          $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-          $string_exp = "/^[A-Za-z .'-]+$/";
-
-          if(!preg_match($email_exp,$email_from)) {
-            $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-          }
-
-          if(!preg_match($string_exp,$first_name)) {
-            $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-          }
-
-          if(!preg_match($string_exp,$last_name)) {
-            $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-          }
-
-          if(strlen($comments) < 2) {
-            $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-          }
-
-          if(strlen($error_message) > 0) {
-            echo '<p class="mailError">' . $error_message . '</p>';
-          }
-          else{
-            $email_message = "Form details below.\n\n";
-       
-            function clean_string($string) {
-              $bad = array("content-type","bcc:","to:","cc:","href");
-              return str_replace($bad,"",$string);
-            }
-       
-            $email_message .= "First Name: ".clean_string($first_name)."\n";
-            $email_message .= "Last Name: ".clean_string($last_name)."\n";
-            $email_message .= "Email: ".clean_string($email_from)."\n";
-            $email_message .= "Telephone: ".clean_string($telephone)."\n";
-            $email_message .= "Comments: ".clean_string($comments)."\n";
-       
-            // create email headers
-            $headers = 'From: '.$email_from."\r\n".
-            'Reply-To: '.$email_from."\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-            @mail($email_to, $email_subject, $email_message, $headers);
-
-            echo '<p class="mailSuccess">Your message has been sent! Thank you!</p>';
-          }
+    <?php
+      if(isset($_POST['email'])) {
+        $email_to = "info@ams360inc.com";
+        $email_subject = "Message from ams360inc.com";
+        
+        // validation expected data exists
+        if(!isset($_POST['first_name']) ||
+          !isset($_POST['last_name']) ||
+          !isset($_POST['email']) ||
+          !isset($_POST['telephone']) ||
+          !isset($_POST['comments'])) {
+          echo '<p class="mailError">We are sorry, but there appears to be a problem with the form you submitted.</p>';
         }
-      ?>
- 
-      <form name="htmlform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <table width="450px">
-          <tr>
-            <td valign="top">
-              <label for="first_name">First Name *</label>
-            </td>
-            <td valign="top">
-              <input  type="text" name="first_name" maxlength="50" size="30">
-            </td>
-          </tr>
- 
-          <tr>
-            <td valign="top"">
-              <label for="last_name">Last Name *</label>
-            </td>
-            <td valign="top">
-              <input type="text" name="last_name" maxlength="50" size="30">
-            </td>
-          </tr>
+   
+        $first_name = $_POST['first_name']; // required
+        $last_name = $_POST['last_name']; // required
+        $email_from = $_POST['email']; // required
+        $telephone = $_POST['telephone']; // not required
+        $comments = $_POST['comments']; // required
+   
+        $error_message = "";
+        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+        $string_exp = "/^[A-Za-z .'-]+$/";
 
-          <tr>
-            <td valign="top">
-              <label for="email">Email Address *</label>
-            </td>
-            <td valign="top">
-              <input  type="text" name="email" maxlength="80" size="30">
-            </td>
-          </tr>
+        if(!preg_match($email_exp,$email_from)) {
+          $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+        }
 
-          <tr>
-            <td valign="top">
-              <label for="telephone">Phone Number</label>
-            </td>
-            <td valign="top">
-              <input type="text" name="telephone" maxlength="30" size="30">
-            </td>
-          </tr>
-          
-          <tr>
-            <td valign="top">
-              <label for="comments">Message *</label>
-            </td>
-            <td valign="top">
-              <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-            </td>
-          </tr>
+        if(!preg_match($string_exp,$first_name)) {
+          $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+        }
 
-          <tr>
-            <td colspan="2" style="text-align:center">
-              <input type="submit" value="Submit">
-            </td>
-          </tr>
-        </table>
-      </form>
-    </div>
+        if(!preg_match($string_exp,$last_name)) {
+          $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+        }
+
+        if(strlen($comments) < 2) {
+          $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+        }
+
+        if(strlen($error_message) > 0) {
+          echo '<p class="mailError">' . $error_message . '</p>';
+        }
+        else{
+          $email_message = "Form details below.\n\n";
+     
+          function clean_string($string) {
+            $bad = array("content-type","bcc:","to:","cc:","href");
+            return str_replace($bad,"",$string);
+          }
+     
+          $email_message .= "First Name: ".clean_string($first_name)."\n";
+          $email_message .= "Last Name: ".clean_string($last_name)."\n";
+          $email_message .= "Email: ".clean_string($email_from)."\n";
+          $email_message .= "Telephone: ".clean_string($telephone)."\n";
+          $email_message .= "Comments: ".clean_string($comments)."\n";
+     
+          // create email headers
+          $headers = 'From: '.$email_from."\r\n".
+          'Reply-To: '.$email_from."\r\n" .
+          'X-Mailer: PHP/' . phpversion();
+          @mail($email_to, $email_subject, $email_message, $headers);
+
+          echo '<p class="mailSuccess">Your message has been sent! Thank you!</p>';
+        }
+      }
+    ?>
+
+    <form name="htmlform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <table width="450px">
+        <tr>
+          <td valign="top">
+            <label for="first_name">First Name *</label>
+          </td>
+          <td valign="top">
+            <input  type="text" name="first_name" maxlength="50" size="30">
+          </td>
+        </tr>
+
+        <tr>
+          <td valign="top"">
+            <label for="last_name">Last Name *</label>
+          </td>
+          <td valign="top">
+            <input type="text" name="last_name" maxlength="50" size="30">
+          </td>
+        </tr>
+
+        <tr>
+          <td valign="top">
+            <label for="email">Email *</label>
+          </td>
+          <td valign="top">
+            <input  type="text" name="email" maxlength="80" size="30">
+          </td>
+        </tr>
+
+        <tr>
+          <td valign="top">
+            <label for="telephone">Phone</label>
+          </td>
+          <td valign="top">
+            <input type="text" name="telephone" maxlength="30" size="30">
+          </td>
+        </tr>
+        
+        <tr>
+          <td valign="top">
+            <label for="comments">Message *</label>
+          </td>
+          <td valign="top">
+            <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="2" style="text-align:center">
+            <input type="submit" value="Submit">
+          </td>
+        </tr>
+      </table>
+    </form>
+
+    <p>To contact sales: Email sales@ams360inc.com</p>
+
+    <p>
+      For business or technical matters please contact: Robert Kobielski, rrk@ams360inc.com<br/>
+      Or call: +1 519 792 3780
+    </p>
   </section>
 
   <footer>
